3f11b6aeaa2cfafb478953090388e13874661fd2
pfn3131-wq <pfn3131@gmail.com>
2026-07-14 08:42:29 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Paharpur Fiber Net & LPG Gas',
+  description: 'পাহাড়পুর বাজার মন্টু মেম্বার এর বাসা ২য় তলা,
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="bn">
+      <head>
+        <meta name="google-site-verification" content="GOHtXGInf5H9JJqjAedrhKYzsU4RNaZpkgCWKpKHIL0" />
+      </head>
       <body>{children}</body>
     </html>
   );
